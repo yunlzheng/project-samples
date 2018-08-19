e837dc3893ae159ca6f459f070fb708da68871c1
yunl.zheng <yunl.zheng@gmail.com>
2018-08-19 22:21:09 +0800

    add sd support

--- spring-cloud-kubernetes-sample/src/main/java/com/github/sample/KubeApplication.java
+++ spring-cloud-kubernetes-sample/src/main/java/com/github/sample/KubeApplication.java
@@ -4,10 +4,15 @@ import com.github.sample.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,9 +22,24 @@ public class KubeApplication {
     @Autowired
     private AppConfig appConfig;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+
     @GetMapping("")
     public ResponseEntity hell() {
         return ResponseEntity.ok(appConfig.getMessage());
+    }
+
+    @GetMapping("/services")
+    public ResponseEntity services() {
+        return ResponseEntity.ok(discoveryClient.getServices());
+    }
+
+    @GetMapping("/services/{name}")
+    public ResponseEntity service(@PathVariable("name") String name) {
+        List<ServiceInstance> instances = discoveryClient.getInstances(name);
+        return ResponseEntity.ok(instances);
     }
 
     public static void main(String[] args) {
