466d41a25c59b35a49105c67e332f98ddeba2214
zhenmu.zyl <zhenmu.zyl@alibaba-inc.com>
2018-08-21 10:13:54 +0800

    add rbac setup

--- spring-cloud-kubernetes-sample/build.sh
+++ spring-cloud-kubernetes-sample/build.sh
@@ -1,1 +1,2 @@
 docker build -t registry.cn-hangzhou.aliyuncs.com/k8s-mirrors/kube-app .
+docker push registry.cn-hangzhou.aliyuncs.com/k8s-mirrors/kube-app
--- spring-cloud-kubernetes-sample/entrypoint.sh
+++ spring-cloud-kubernetes-sample/entrypoint.sh
@@ -1,1 +1,2 @@
-java -Xmx512m -Djava.security.egd=file:/dev/./urandom -jar spring-cloud-kubernetes-sample.jar --spring.profiles.active=${ACTIVE_PROFILE} $@
+#!/usr/bin/env bash
+java -Xmx512m -Djava.security.egd=file:/dev/./urandom -jar spring-cloud-kubernetes-sample.jar $@
